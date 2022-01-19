87c50849e9312ab72c3b3bcf429fb0cc00208387
Dave Kiss <dave@davekiss.com>
2022-01-19 10:43:07 -0500

    Add proxy for API requests

---
+++ src/setupProxy.js
@@ -0,0 +1,31 @@
+/**
+ * We need to have a server-side solution
+ * to make API requests in place because it is not secure to use your
+ * Mux credentials directly in the browser environment.
+ *
+ * This proxy acts as a fake server to handle client-side API requests
+ * and forward them on to Mux.
+ *
+ * Make sure to add your Mux API access credentials to a `.env` file
+ * located at the root directory of this project.
+ */
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const publicKey = process.env.REACT_APP_MUX_ACCESS_TOKEN_ID;
+const secretKey = process.env.REACT_APP_MUX_SECRET_KEY;
+const auth = btoa(`${publicKey}:${secretKey}`);
+
+module.exports = function (app) {
+  app.use(
+    '/my-api/**',
+    createProxyMiddleware({
+      target: 'https://api.mux.com',
+      pathRewrite: (path, req) => path.replace('/my-api', ''),
+      changeOrigin: true,
+      headers: {
+        "Authorization": `Basic ${auth}`,
+        "Content-Type": "application/json",
+      }
+    })
+  );
+};
