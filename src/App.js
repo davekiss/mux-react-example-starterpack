41204cd1afc100bebd9963eef06e70a8228118ee
Dave Kiss <dave@davekiss.com>
2022-02-04 13:50:02 -0500

    Pipe realtime data to chart

--- src/App.js
+++ src/App.js
@@ -1,29 +1,76 @@
-import { useEffect, useState } from "react";
+import './App.css';
+import { useEffect, useState, useCallback } from "react";
 import Bar from "./Bar";
+import MuxVideo from "@mux-elements/mux-video-react";
 
-const assetId = 'CJJ8nFi8eWRc6x00OWWmxD028596ufz01pTV4snVzgqr4g';
+import { differenceInMinutes, startOfMinute } from 'date-fns'
 
 function App() {
-  const [data, setData] = useState([]);
+  const [currentViewers, setCurrentViewers] = useState();
+  const [historicalViewers, setHistoricalViewers] = useState();
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      fetch(`/stats?assetId=${assetId}`)
-        .then(resp => resp.json())
-        .then(payload => {
-          console.log(payload);
-          setData(prevState => [...prevState, payload.data]);
-        });
-    }, 10000);
+  const getViewers = useCallback(async () => {
+    const response = await fetch(`/my-api/data/v1/realtime/metrics/current-concurrent-viewers/timeseries`)
+    const payload = await response.json();
 
-    return () => clearInterval(timer);
+    const rows = payload.data;
+    const currentValue = rows[rows.length - 1].value;
+
+    let historicalValues = {};
+
+    rows.forEach(row => {
+      const date = startOfMinute(new Date(row.date));
+      const delta = differenceInMinutes(date, startOfMinute(new Date()));
+
+      const label = `${delta}m`;
+      // set if it doesn't exist, or, if it is less than the incoming value
+      if (!historicalValues[label] || historicalValues[label] < row.value) {
+        historicalValues[label] = row.value;
+      }
+    });
+
+    // massage into a shape compatible with react-charts
+    historicalValues = Object.keys(historicalValues).map(key => ({
+      label: key,
+      value: historicalValues[key]
+    }))
+
+    return { currentValue, historicalValues }
   }, []);
 
-  console.log(data);
+  const refreshValues = useCallback(async () => {
+    const { currentValue, historicalValues } = await getViewers();
+    setCurrentViewers(currentValue);
+    setHistoricalViewers(historicalValues);
+    console.dir(historicalValues);
+  }, [getViewers]);
+
+  useEffect(() => {
+    refreshValues();
+    const timer = setInterval(refreshValues, 5000);
+    return () => clearInterval(timer);
+  }, [refreshValues]);
 
   return (
     <div>
-      <Bar />
+      <div className="p-10 shadow m-10 rounded w-min bg-blue-600 text-white">
+        <h1 className="text-sm">Active viewers right now</h1>
+        <span className="text-5xl mb-10 block">{currentViewers}</span>
+
+        {historicalViewers && <Bar historicalViewers={historicalViewers} />}
+      </div>
+
+      <MuxVideo
+        playbackId="yVvUsu1ON3vuBRzfTBucWQzT96702ey02x5s1HR9nmcyA"
+        metadata={{
+          video_id: "video-id-123456",
+          video_title: "Super Interesting Video",
+          viewer_user_id: "user-id-bc-789",
+        }}
+        envKey="huqs2grm9q4lq9bifqeou30g4"
+        streamType="on-demand"
+        controls
+      />
     </div>
   );
 }
--- src/Bar.js
+++ src/Bar.js
@@ -1,58 +1,18 @@
 import React, { useMemo } from "react";
 import { Chart } from "react-charts";
 
-export default function Bar() {
+export default function Bar({ historicalViewers }) {
   const data = useMemo(() => [
     {
       label: 'Real-time viewers',
-      data: [
-        {
-          time: "-10m",
-          viewers: 6,
-        },
-        {
-          time: "-9m",
-          viewers: 12,
-        },
-        {
-          time: "-8m",
-          viewers: 20,
-        },
-        {
-          time: "-7m",
-          viewers: 19,
-        },
-        {
-          time: "-6m",
-          viewers: 23,
-        },
-        {
-          time: "-5m",
-          viewers: 6,
-        },
-        {
-          time: "-4m",
-          viewers: 12,
-        },
-        {
-          time: "-3m",
-          viewers: 20,
-        },
-        {
-          time: "-2m",
-          viewers: 19,
-        },
-        {
-          time: "-1m",
-          viewers: 23,
-        }
-      ],
+      data: historicalViewers
     },
-  ], []);
+  ], [historicalViewers]);
 
   const primaryAxis = React.useMemo(
     () => ({
-      getValue: datum => datum.time,
+      getValue: datum => datum.label,
+      showGrid: false
     }),
     []
   )
@@ -60,21 +20,24 @@ export default function Bar() {
   const secondaryAxes = React.useMemo(
     () => [
       {
-        getValue: datum => datum.viewers,
+        getValue: datum => datum.value,
         min: 0,
-        max: 30
+        max: historicalViewers.reduce((prev, current) => current.value > prev.value ? current : prev).value * 1.1,
+        show: false
       },
     ],
-    []
+    [historicalViewers]
   )
 
   return (
-    <div style={{ width: "300px", height: "150px", margin: "50px" }}>
+    <div style={{ width: "400px", height: "150px" }}>
       <Chart
         options={{
           data,
           primaryAxis,
-          secondaryAxes
+          secondaryAxes,
+          dark: true,
+          defaultColors: ["#93c5fd"]
         }}
       />
     </div>
